06706ada215955bc3fcc61f46f960a98dee71717
Étienne Letarte <50108815+EtsyLeGrand@users.noreply.github.com>
2022-09-22 20:03:35 -0400

    Cours Graphics

--- Hambourgeois/Hambourgeois/sources/Engine.cpp
+++ Hambourgeois/Hambourgeois/sources/Engine.cpp
@@ -1,29 +1,40 @@
 #include <Engine.h>
 #include <SDL.h>
+#include <SDL_image.h>
 #include <time.h>
 #include <Windows.h>
 #include <SdlInput.h>
 #include <ConsoleLogger.h>
 #include <FileLogger.h>
 #include <iostream>
+#include <vld.h>
 
 static SDL_Renderer* _renderer = nullptr;
 static SDL_Window* _window = nullptr;
 static unsigned char const* _keys = nullptr;
 static bool isRunning = false;
 
-#define TARGET_FPS 60.0
+static SDL_Texture* dkTexture = nullptr;
 
-static float const MS_PER_FRAME = 1000 / TARGET_FPS;
+static const float TARGET_FPS = 60.0f;
+static const float MS_PER_FRAME = (1000 / TARGET_FPS);
 
 static float xPos = 0;
 static float yPos = 0;
 
 bool hambourgeois::Engine::Init(const std::string& title, int w, int h)
 {
+#if _DEBUG
+	logger = new ConsoleLogger();
+#else
+	logger = new FileLogger();
+#endif
+
+	logger->Log("Initialization complete");
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
-		SDL_Log(SDL_GetError());
+		logger->Log(SDL_GetError());
 		return false;
 	}
 
@@ -35,7 +46,7 @@ bool hambourgeois::Engine::Init(const std::string& title, int w, int h)
 
 	if (!_window)
 	{
-		SDL_Log(SDL_GetError());
+		logger->Log(SDL_GetError());
 		return false;
 	}
 
@@ -50,19 +61,17 @@ bool hambourgeois::Engine::Init(const std::string& title, int w, int h)
 
 	input = new SdlInput();
 	
-#if !_DEBUG
-	logger = new ConsoleLogger();
-#else
-	logger = new FileLogger();
-#endif
-	logger->Log("Initialization complete");
 	return true;
 }
 
 void hambourgeois::Engine::Start()
 {
 	isRunning = true;
+
+	dkTexture = IMG_LoadTexture(_renderer, "ressources/images/dk.png");
+
 	clock_t lastTime = clock();
+
 	while (isRunning)
 	{
 		const clock_t start = clock();
@@ -72,7 +81,8 @@ void hambourgeois::Engine::Start()
 		Update(dt);
 		Render();
 
-		Sleep(MS_PER_FRAME - dt);
+		float sleepAmount = start + (MS_PER_FRAME - clock());
+		if (sleepAmount > 0) Sleep(static_cast<DWORD>(sleepAmount));
 
 		lastTime = start;
 	}
@@ -122,13 +132,19 @@ void hambourgeois::Engine::Render()
 	SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
 	SDL_RenderClear(_renderer);
 
-	SDL_SetRenderDrawColor(_renderer, 255, 0, 0, 255);
+	SDL_Rect initialRect = { 1 + (47 * 0), 1, 44, 32 }; // +47
 
+	SDL_Rect destRect = { 100, 100, 88, 64 };
+	
+	SDL_RenderCopyEx(_renderer, dkTexture, &initialRect, &destRect, 0, NULL, SDL_FLIP_NONE);
+
+	SDL_SetRenderDrawColor(_renderer, 255, 0, 0, 255);
+	
 	int w, h;
 
 	SDL_GetWindowSize(_window, &w, &h);
 
-	SDL_Rect rect = { (w / 2 - 25) + xPos, (h / 2 - 25) + yPos, 50, 50 };
+	SDL_Rect rect = { static_cast<int>((w / 2 - 25) + xPos), static_cast<int>((h / 2 - 25) + yPos), 50, 50 };
 
 	SDL_RenderFillRect(_renderer, &rect);
 
